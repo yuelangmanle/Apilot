f6f5d054b75e9e9c3b3778a916ab9047d45ea7e8
月亮满了 <yueliangmanle@yueliangmanledeMacBook-3.local>
2026-05-29 16:51:14 +0800

    fix: 更新 compileSdk 为 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.example.api_manager"
-    compileSdk = 34
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -24,7 +24,7 @@ android {
     defaultConfig {
         applicationId = "com.example.api_manager"
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
